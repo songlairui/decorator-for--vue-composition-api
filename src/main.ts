ca1a019bfcbe693be8dd55efcf9068b77ffee1e1
songlairui <songlairui@126.com>
2020-06-06 14:49:50 +0800

    feat: add composition-api

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+import VueCompositionApi from "@vue/composition-api";
+
+Vue.use(VueCompositionApi);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
